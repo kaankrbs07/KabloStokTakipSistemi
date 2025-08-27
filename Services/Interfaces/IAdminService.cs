ff0589eadba2c770186540043ce2da5f1473cf72
Enes kaan Karabaş <153426680+kaankrbs07@users.noreply.github.com>
2025-08-27 09:35:05 +0300

    Update IAdminService

--- Services/Interfaces/IAdminService.cs
+++ Services/Interfaces/IAdminService.cs
@@ -7,9 +7,6 @@ public interface IAdminService
     Task<IEnumerable<GetAdminDto>> GetAllAdminsAsync();
     Task<GetAdminDto?> GetAdminByIdAsync(long adminId);
 
-    // Users için ayrı DTO (CreateUserDto) zaten var; Admins için CreateAdminDto
     Task<bool> CreateAdminAsync(CreateUserDto userDto, CreateAdminDto adminDto);
-
-    // DepartmentName artık string
     Task<bool> UpdateAdminDepartmentAsync(long adminId, string newDepartmentName);
 }
